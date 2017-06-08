80acb736b6fa11b69e7a2abd95906c4b5a8d1cba
dfdf <tejo@dfd.com>
2017-06-08 11:11:59 +0000

    scss components added

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,5 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss','pubic/css/style.css');
+    mix.sass('/resources/scss/global.scss','/pubic/css/style.css');
 });
